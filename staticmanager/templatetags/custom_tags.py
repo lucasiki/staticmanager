34d0731d638c57123b413667dfb690524ed9f575
lucasiki <46225549+lucasiki@users.noreply.github.com>
2022-06-26 20:16:57 -0300

    Update custom_tags.py

--- staticmanager/templatetags/custom_tags.py
+++ staticmanager/templatetags/custom_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from excelupload.models import *
+from barber import settings
 
 register = template.Library()
 
@@ -16,3 +16,34 @@ def get_image(value, arg):
         return value[arg]['value']
     else:
         return ''
+    
+@register.filter()
+def staticmanager(value, arg):
+    try:
+        arg = arg.split(':')
+        obj = staticManager.objects.filter(keyobj=arg[0])[0]
+        returner=''
+        if obj.type == 1:
+            returner = obj.textobj
+        if obj.type == 2:
+            returner = obj.textareaobj
+        if obj.type == 3:
+            returner = settings.MEDIA_URL + obj.fileobj
+        if obj.type == 4:
+            if arg[1] == 'h':
+                returner = obj.linkobjh
+
+            else:
+                returner = obj.linkobjb
+
+
+        return returner
+    except:
+        try: 
+            value2 = value.split(':')
+            if value2[1] == 'static':
+                value = settings.STATIC_URL + value2[0]
+                print(value)
+        except:
+            pass
+    return value
