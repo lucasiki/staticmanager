e4cfb6d62b32eab6b75361468e6431ddd3540b5d
lucasiki <46225549+lucasiki@users.noreply.github.com>
2022-07-22 08:45:14 -0300

    Update urls.py

--- staticmanager/urls.py
+++ staticmanager/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", staticManagerView.as_view(), name="staticManager"),
     path("<slug:Slug>", staticManagerView.as_view(), name="staticManager-search"),
     path("fetchCategory/", categoryView.as_view(), name="fetch-category"),
+    path("switch-staticmanager/", toggleManager.as_view(), name="switch-staticmanager"),
 ]
